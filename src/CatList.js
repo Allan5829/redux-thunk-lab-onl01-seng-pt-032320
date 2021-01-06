39149a537567e6b7396df77188cef26452c540cd
Allan5829 <allan.farca@gmail.com>
2021-01-06 03:44:05 -0800

    meets functionality requirements but doesn't pass tests.

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,11 @@
-// write your CatList component here
+import React from 'react';
+
+const CatList = props => (
+    <div> 
+        {props.cats.map( (cat, i) => {
+            return <img key={i} src={cat.url} alt="Loading Cat Pic" width="200" height="200"/>
+        })} 
+    </div> 
+)  
+
+export default CatList
